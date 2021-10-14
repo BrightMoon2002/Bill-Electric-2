d9fee74a9b78224fd6f2b637efa82044447e4cdf
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-10-14 18:51:57 +0700

    Add read and write file customer list

---
+++ src/storage/FileManagerCustomer.java
@@ -0,0 +1,52 @@
+package storage;
+
+import model.Customer;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManagerCustomer {
+    public List<Customer> readListCustomer() {
+        List<Customer> customerList = new ArrayList<>();
+        File file = new File("CustomerList.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (file.length() == 0) {
+            return customerList;
+        }
+        try {
+            InputStream is = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(is);
+            customerList = (List<Customer>) ois.readObject();
+            ois.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return customerList;
+    }
+
+    public static void writeFile(List<Customer> customerList) {
+        File file = new File("CustomerList.txt");
+        try {
+            OutputStream os = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(customerList);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
